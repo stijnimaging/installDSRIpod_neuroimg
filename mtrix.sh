2f28a56619fd005c80a4a39fc9c27e9b680fd2b9
Stijn <stijn.imaging@gmail.com>
2022-05-09 12:45:45 +0200

    Create mtrix.sh

---
+++ mtrix.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd /root
+mkdir installers
+cd installers
+
+apt-get install git g++ python libeigen3-dev zlib1g-dev libqt5opengl5-dev libqt5svg5-dev libgl1-mesa-dev libfftw3-dev libtiff5-dev libpng-dev
+git clone https://github.com/MRtrix3/mrtrix3.git
+cd mrtrix3
+./configure
+./build
