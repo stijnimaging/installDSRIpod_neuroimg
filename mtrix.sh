21a7fb78a1feb8e0c08fbd29ccffabb843f4f975
Stijn <stijn.imaging@gmail.com>
2022-05-09 13:14:46 +0200

    Update mtrix.sh

--- mtrix.sh
+++ mtrix.sh
@@ -3,7 +3,9 @@
 cd /root
 mkdir installers
 cd installers
-
+apt-get install -y kate gedit 
+apt-get install -y dirmngr gnupg apt-transport-https ca-certificates software-properties-common
+apt-get install -y r-base
 apt-get install -y git g++ python libeigen3-dev zlib1g-dev libqt5opengl5-dev libqt5svg5-dev libgl1-mesa-dev libfftw3-dev libtiff5-dev libpng-dev
 git clone https://github.com/MRtrix3/mrtrix3.git
 cd mrtrix3
