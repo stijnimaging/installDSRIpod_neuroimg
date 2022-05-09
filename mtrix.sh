bc7c200f426e09ea0e62e101d0eccba131a32b9b
Stijn <stijn.imaging@gmail.com>
2022-05-09 12:46:29 +0200

    Update mtrix.sh

--- mtrix.sh
+++ mtrix.sh
@@ -4,7 +4,7 @@ cd /root
 mkdir installers
 cd installers
 
-apt-get install git g++ python libeigen3-dev zlib1g-dev libqt5opengl5-dev libqt5svg5-dev libgl1-mesa-dev libfftw3-dev libtiff5-dev libpng-dev
+apt-get install -y git g++ python libeigen3-dev zlib1g-dev libqt5opengl5-dev libqt5svg5-dev libgl1-mesa-dev libfftw3-dev libtiff5-dev libpng-dev
 git clone https://github.com/MRtrix3/mrtrix3.git
 cd mrtrix3
 ./configure
